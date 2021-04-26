30aadc09f4426b406bae879c9f13700fa0732db9
JONNATHAN MARCO AURELIO LANUZA GALVAN <jmlanuza@correo.url.edu.gt>
2021-04-25 23:09:23 -0600

    b

--- Controllers/HashController.cs
+++ Controllers/HashController.cs
@@ -7,7 +7,6 @@ using System.Web;
 using System.Threading.Tasks;
 using System.IO;
 using Lab4_CésarSilva1184519_JonnathanLanuza1082219.Models.Data;
-using Lab4_CésarSilva1184519_JonnathanLanuza1082219.Models;
 using System.Threading;
 using Microsoft.AspNetCore.Hosting;
 
@@ -15,14 +14,68 @@ namespace Lab4_CésarSilva1184519_JonnathanLanuza1082219.Controllers
 {
     public class HashController : Controller
     {
-        //List<Developer> MDeveloperList = new List<Developer>();
-        //List<DevData> MDevDataList = new List<DevData>();
-        //int[,] array = new int[25, 25];
+        List<string> MDeveloperList = new List<string>();
+        List<string> MDevDataList = new List<string>();
+        int[,] array = new int[25, 25];
 
         // GET: HashController
         public ActionResult Index()
         {
             return View(Singleton.Instance.MDeveloperList);
+        }
+
+        public ActionResult CreateDev()
+        {
+            return View();
+        }
+
+        public ActionResult CreateDevTask()
+        {
+            return View();
+        }
+
+        //[HttpPost]
+        //public ActionResult Index(string Title)
+        //{
+        //    ViewData["SearchName"] = Title;
+        //    Singleton.Instance.MDeveloperList.Clear();
+
+        //    if (Title != null)
+        //    {
+        //        for (int i = 0; i < Singleton.Instance.MDeveloperList.Count() - 1; i++)
+        //        {
+        //            if (Singleton.Instance.MDeveloperList[i].Title == Title)
+        //            {
+        //                Singleton.Instance.MDeveloperList.Add(Singleton.Instance.MDeveloperList[i]);
+        //            }
+        //        }
+        //        return View(Singleton.Instance.MDeveloperList);
+        //    }
+        //    return View();
+        //}
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Index(IFormCollection collection)
+        {
+            try
+            {
+                var newdev = new Models.Developer
+                {
+                    Title = collection["Title"],
+                    Description = collection["Description"],
+                    Proyect = collection["Proyect"],
+                    Priority = Convert.ToInt32(collection["Priority"]),
+                    DueDate = collection["DueDate"]
+                };
+                MDeveloperList.Add(newdev.ToString());
+                Singleton.Instance.MDeveloperList.Add(newdev);
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
         }
 
         // GET: HashController/Details/5
@@ -31,7 +84,7 @@ namespace Lab4_CésarSilva1184519_JonnathanLanuza1082219.Controllers
             return View();
         }
 
-        // GET: HashController/Create
+        // GET: HashController/CreateDev
         public ActionResult Create()
         {
             return View();
