1414076f1e29621f803841586602d84b31f337e2
JONNATHAN MARCO AURELIO LANUZA GALVAN <jmlanuza@correo.url.edu.gt>
2021-04-25 21:24:48 -0600

    cosas

--- Models/Developer.cs
+++ Models/Developer.cs
@@ -9,7 +9,7 @@ namespace Lab4_CésarSilva1184519_JonnathanLanuza1082219.Models
     {
         public string Title { get; set; }
         public string Description { get; set; }
-        public string Proyect { get; set; }
+        public string Project { get; set; }
         public int? Priority { get; set; }
         public string DueDate { get; set; }
     }
